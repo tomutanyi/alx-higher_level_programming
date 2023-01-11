64f60b24c4c8605a8da3e2ba940c1d9a62d36da1
tomutanyi <94693960+tomutanyi@users.noreply.github.com>
2023-01-11 14:57:27 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    5_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (5_list)
+    q_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (q_list)
