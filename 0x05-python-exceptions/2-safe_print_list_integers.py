5448d6abbd899cdb3d05a4a29cc910f2cc5a461f
tomutanyi <94693960+tomutanyi@users.noreply.github.com>
2023-01-24 16:23:14 +0300

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,7 +11,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except ValueError:
             pass
         else:
-            count += 1
+            dooku += 1
 
     print()
     return (dooku)
