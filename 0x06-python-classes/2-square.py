609ef5876e99a6ad5e66ccdf3528a03bc0f0ac9b
tomutanyi <tomutanyi@gmail.com>
2023-01-25 06:00:09 -0800

    2 more tasks

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+class Square():
+    """square class with it's size and proper validations ."""
+
+    def __init__(self, size=0):
+
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+
+        elif (size < 0):
+
+            raise ValueError("size must be >= 0")
+        self.__size = size
